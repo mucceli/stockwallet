896e9015c8e5ac7707f8d815ee2b9cb80a654048
mucceli <mucceli@gmail.com>
2019-02-07 11:29:05 -0200

    Fit new config

--- src/controller/acoes/CrudStocksMB.java
+++ src/controller/acoes/CrudStocksMB.java
@@ -3,8 +3,10 @@ package controller.acoes;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import dao.StocksDAO;
 import enums.Liquidity;
@@ -24,11 +26,23 @@ public class CrudStocksMB implements Serializable {
 		entity = new Stock();
 	}
 	public String saveEntity() {
+		try{
+			checkNotNull(entity);
+		}catch (IllegalArgumentException e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
+		}
 		entity.setLiquidity(liquiditySelected);
 		new StocksDAO().save(entity);
+		//Passar mensagem de sucesso.
 		return "stocks";
 	}
 
+	private void checkNotNull(Stock entity2) {
+		if(entity2 == null || entity2.getName() == null || entity2.getSymbol() == null) {
+			throw new IllegalArgumentException();
+		}
+		
+	}
 	public Liquidity[] getAllLiquiditys() {
 		return Liquidity.values();
 	}
