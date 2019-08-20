e83d9e95b92e627be8c2311c5fad2ca7b96d7bf1
mucceli <mucceli@gmail.com>
2019-08-20 00:07:13 -0300

    Enhancement UC Stocks

--- src/main/java/controller/acoes/CrudStocksMB.java
+++ src/main/java/controller/acoes/CrudStocksMB.java
@@ -3,7 +3,6 @@ package controller.acoes;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -11,6 +10,7 @@ import javax.faces.context.FacesContext;
 import dao.StocksDAO;
 import enums.Liquidity;
 import model.Stock;
+import util.StockWalletConstants;
 
 @ManagedBean(name="crudStocksMB")
 @ViewScoped
@@ -26,23 +26,20 @@ public class CrudStocksMB implements Serializable {
 		entity = new Stock();
 	}
 	public String saveEntity() {
-		try{
-			checkNotNull(entity);
-		}catch (IllegalArgumentException e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
-		}
 		entity.setLiquidity(liquiditySelected);
 		new StocksDAO().save(entity);
-		//Passar mensagem de sucesso.
-		return "stocks";
+		
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(StockWalletConstants.CALLBACK, StockWalletConstants.CALLBACK_SUCCESS);
+		return "stocks.xhtml";
 	}
 
-	private void checkNotNull(Stock entity2) {
-		if(entity2 == null || entity2.getName() == null || entity2.getSymbol() == null) {
-			throw new IllegalArgumentException();
-		}
-		
-	}
+//	private void checkNotNull(Stock entity2) {
+//		if(entity2 == null || entity2.getName() == null || entity2.getSymbol() == null) {
+//			throw new IllegalArgumentException();
+//		}
+//		
+//	}
+	
 	public Liquidity[] getAllLiquiditys() {
 		return Liquidity.values();
 	}
--- src/main/java/controller/acoes/StocksMB.java
+++ src/main/java/controller/acoes/StocksMB.java
@@ -2,12 +2,17 @@ package controller.acoes;
 
 import java.util.ArrayList;
 
-import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Inject;
 
 import dao.StocksDAO;
 import model.Stock;
+import util.FacesContextUtil;
+import util.StockWalletConstants;
 
 @ManagedBean(name="stocksMB")
 @ViewScoped
@@ -15,12 +20,17 @@ public class StocksMB {
 	
 	public ArrayList<Stock> stockList;
 	
-	public String login() {
-		return "stocks.xhtml";
-	}
+	@Inject
+	private FacesContextUtil facesContextUtil;
 	
-	@PostConstruct
-	public void init() {
+	public void init(ComponentSystemEvent event) {
+		
+		//Show message after action in CRUD.
+		if(StockWalletConstants.CALLBACK_SUCCESS.equals(facesContextUtil.getExternalContext().getSessionMap().get(StockWalletConstants.CALLBACK))) {
+			FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Stock saved with success!");
+			FacesContext.getCurrentInstance().addMessage("message", facesMessage);
+		}
+		
 		stockList = new StocksDAO().findAll();
 	}
 	
